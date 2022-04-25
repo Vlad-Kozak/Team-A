a89d4f73331ac3f48ca54d71a04e69c9160c1cec
Vlad-Kozak <k911k12@gmail.com>
2022-04-25 10:38:43 +0300

    add animation card

--- src/js/product-btn.js
+++ src/js/product-btn.js
@@ -39,3 +39,45 @@
     mobilMenuRef.classList.toggle('is-open');
   });
 })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-item1]');
+//   const mobilMenuRef = document.querySelector('[data-item1]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobilMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-item2]');
+//   const mobilMenuRef = document.querySelector('[data-item2]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobilMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-item3]');
+//   const mobilMenuRef = document.querySelector('[data-item3]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//     mobilMenuRef.classList.toggle('is-open');
+//   });
+// })();
