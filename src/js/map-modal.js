e23124f159d15c48f5375da1528b0dcecad23d2e
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-04-27 11:08:52 +0300

    in process

---
+++ src/js/map-modal.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[map-modal-open]"),
+    closeModalBtn: document.querySelector("[map-modal-close]"),
+    modal: document.querySelector("[map-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
